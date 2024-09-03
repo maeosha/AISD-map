ee3357b40e19e211515114c31cd2f7685773962d
maeosha <mochkauha123@gmail.com>
2024-09-03 15:32:28 +0400

    Implement Hashmap constructors and destructor

--- main.cpp
+++ main.cpp
@@ -82,4 +82,35 @@ Key Bucket<Val, Key>::get_key() const {
     return key;
 }
 
+template <typename Val, typename Key, typename Container>
+Hashmap<Val, Key, Container>::Hashmap() : size(0), hash_data(32, nullptr) {}
+
+template <typename Val, typename Key, typename Container>
+Hashmap<Val, Key, Container>::Hashmap(const Hashmap<Val, Key, Container>& other)
+        : size(other.size), hash_data(other.hash_data.size(), nullptr), rand_constant(other.rand_constant) {
+    for (size_t i = 0; i < other.hash_data.size(); ++i) {
+        if (other.hash_data[i]) {
+            hash_data[i] = new Container(*other.hash_data[i]);
+        }
+    }
+}
+
+template <typename Val, typename Key, typename Container>
+Hashmap<Val, Key, Container>::Hashmap(size_t initial_size, Val min_value, Val max_value, Key min_key, Key max_key) {
+    if (initial_size == 0) throw std::invalid_argument("Size = 0");
+    hash_data.resize(initial_size, nullptr);
+    for (size_t i = 0; i < initial_size; ++i) {
+        Val rand_value = generate_random_real_number(min_value, max_value);
+        Key rand_key = generate_random_real_number(min_key, max_key);
+        insert(rand_value, rand_key);
+    }
+}
+
+template <typename Val, typename Key, typename Container>
+Hashmap<Val, Key, Container>::~Hashmap() {
+    for (auto& bucket_list_ptr : hash_data) {
+        delete bucket_list_ptr;
+    }
+    hash_data.clear();
+}
 
